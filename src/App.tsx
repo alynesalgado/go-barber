e8e27400cd56d96e15b349c304099426b843e48a
Alyne Salgado Alves <alynesalgadoalves@gmail.com>
2020-09-22 20:52:46 -0300

    adding auth, toast and more thing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-
-// import SignIn from './pages/SignIn';
-import SingUp from './pages/SignUp';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
 
+import AppProvider from './hooks';
+
+import Routes from './routes';
+
 const App: React.FC = () => (
-  <>
-    <SingUp />
+  <Router>
+    <AppProvider>
+      <Routes />
+    </AppProvider>
     <GlobalStyle />
-  </>
+  </Router>
 );
 
 export default App;
